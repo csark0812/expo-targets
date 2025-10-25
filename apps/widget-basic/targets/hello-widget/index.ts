8b688825335f8a43f1ac14794e7dccf8c6b0746b
csark0812 <csark0812@gmail.com>
2025-10-24 17:47:39 -0700

    Remove widget-basic app and associated files to streamline the project structure and eliminate unused components.

--- apps/widget-basic/targets/hello-widget/index.ts
+++
@@ -1,8 +0,0 @@
-import { createTarget } from 'expo-targets';
-
-export const HelloWidget = createTarget('hello-widget');
-
-export type HelloWidgetData = {
-  message: string;
-  count?: number;
-};
